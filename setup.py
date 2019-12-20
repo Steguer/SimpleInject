e57af6a8c0039b6e2abfec913044f80f3317f3ab
Steven GERARD <steven.gerard.deruelle@gmail.com>
2019-12-20 14:15:15 -0500

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='simpleinject',
+    version='1.0.0',
+    author="Steven Gerard",
+    description="A simple service provider for Python using service injection.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Steguer/SimpleInject",
+    packages=["simpleinject"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+ )
