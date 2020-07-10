e0e676cd454533ba51f2a08eeac7816a44770265
Steven GERARD <steven.gerard.deruelle@gmail.com>
2020-07-10 08:42:33 -0400

    chore: Update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='simpleinject',
-    version='1.1.0',
+    version='1.1.1',
     author="Steven Gerard",
     description="A simple service provider for Python using service injection.",
     long_description=long_description,
